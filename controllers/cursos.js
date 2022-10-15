7557c797b260904e7c22bfa3067550c3d6b6710b
kevin.moreno249 <kevin.moreno249@misena.edu.co>
2022-10-15 17:43:05 -0500

    Separando las funciones asincronas a los ficheros correspondientes en el directorio LOGIC. Exportando las funciones. Refactorizando los controladores

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -1,27 +1,25 @@
 const express = require('express');
-const Curso = require('../models/curso_model');
+const logic = require('../logic/curso_logic');
 const ruta = express.Router();
 
 
 
 
-module.exports = ruta;
 
-
-//Funcion asincronica para crear cursos
-async function crearCurso(body){
-    let curso = new Curso({
-        titulo : body.titulo,
-        descripcion : body.descripcion,
-        alumnos : body.alumnos,
-        calificacion : body.calificacion
-    });
-    return await curso.save();
-}
+//Endpoint Listar CURSOS
+ruta.get('/', (req,res) => {
+    let resultado = logic.listarCursosActivos();
+    resultado.then(cursos => {
+     res.json(cursos);
+    }).catch(err => {
+     res.status(400).json(err);
+     });
+ });
+ 
 
 //Endpoint de tipo POST para el recurso de CURSOS
 ruta.post('/', (req, res) =>{
-    let resultado = crearCurso(req.body);
+    let resultado = logic.crearCurso(req.body);
 
     resultado.then(curso => {
         res.json({
@@ -34,20 +32,10 @@ ruta.post('/', (req, res) =>{
     })
 });
 
-//funcion asincronica para actualizar cursos
-async function actualizarCursos(id, body){
-    let curso = await Curso.findByIdAndUpdate(id,{
-        $set: {
-            titulo: body.titulo,
-            descripcion: body.descripcion
-        }
-    },{new: true});
-    return curso;
-}
 
 //Endpoitn de tipo PUT para el recurso de CURSOS
 ruta.put('/:id', (req, res)=> {
-    let resultado = actualizarCursos(req.params.id, req.body);
+    let resultado = logic.actualizarCursos(req.params.id, req.body);
     resultado.then(curso => {
         res.json(curso)
     }).catch(err => {
@@ -55,19 +43,10 @@ ruta.put('/:id', (req, res)=> {
     })
 })
 
-//funcion asincronica para inactivar cursos
-async function desactivarCurso(id){
-    let curso = await Curso.findByIdAndUpdate(id,{
-        $set:{
-            estado: false
-        }
-    },{new: true});
-    return curso;
-}
 
 //Endpoint de tipo DELETE para el recurso de CURSOS
 ruta.delete('/:id', (req, res) =>{
-    let resultado = desactivarCurso(req.params.id);
+    let resultado = logic.desactivarCurso(req.params.id);
 
     resultado.then(curso => {
         res.json(curso);
@@ -77,18 +56,5 @@ ruta.delete('/:id', (req, res) =>{
 });
 
 
-//funcion asincronica para listar los cursos activos
-async function listarCursosActivos(){
-    let cursos = await Curso.find({"estado":true});
-    return cursos;
-}
 
-//Endpoint Listar CURSOS
-ruta.get('/', (req,res) => {
-   let resultado = listarCursosActivos();
-   resultado.then(cursos => {
-    res.json(cursos);
-   }).catch(err => {
-    res.status(400).json(err);
-   })
-});
+module.exports = ruta;
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,46 +1,32 @@
 const express = require('express');
-const Usuario = require('../models/usuario_model');
-const Joi = require('@hapi/joi');
+const logic = require('../logic/usuario_logic');
 const ruta = express.Router();
 
 
+  
 
 
-module.exports = ruta;
+//Endpoint de Tipo GET para el recurso usuarios. lista todos los usuarios activos
+ruta.get('/',(req, res) => {
+    let resultado = logic.listarUsuarioActivos();
+    resultado.then(usuarios => {
+        res.json(usuarios)
+    }).catch(err => {
+        res.status(400).json(
+            {
+                err
+            }
+        )
+    })
+})
 
-//validaciondes para el objeto usuario
-const schema = Joi.object({
-    nombre: Joi.string()
-      .min(3)
-      .max(30)
-      .required()
-      .pattern(/^[A-Za-záéíóú ]{3,30}$/),
-      
-    password: Joi.string()
-      .pattern(/^[a-zA-z0-9]{3,30}$/),
-  
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu', 'co']} })
-  });
-  
-  //funcion asincrona para crear un objeto de tipo usuario
-  async function crearUsuario(body){
-      let usuario = new Usuario(
-          {
-              email : body.email,
-              nombre : body.nombre,
-              password : body.password
-          });
-          return await usuario.save();
-  }
-  
-  //Endpoint de tipo POST para el recurso de USUARIOS
+//Endpoint de tipo POST para el recurso de USUARIOS
   ruta.post('/', (req, res) => {
       let body = req.body;
   
-      const {error, value} = schema.validate({nombre: body.nombre, email: body.email});
+      const {error, value} = logic.schema.validate({nombre: body.nombre, email: body.email});
       if(!error){
-          let resultado = crearUsuario(body);
+          let resultado = logic.crearUsuario(body);
           
           resultado.then( user => {
               res.json({
@@ -58,24 +44,13 @@ const schema = Joi.object({
       }   
   });
 
-//funcion asincronica para actualizar un usuario
-  async function actualizarUsuario(email,  body)
-  {
-        let usuario = await Usuario.findOneAndUpdate({"email": email},
-        {
-            $set: {
-                    nombre: body.nombre,
-                    password: body.password
-            }
-        }, {new: true});
-        return usuario;
-  }
 
-  //Endpoint de tipo Put para actualizar los datos del usuario
+
+//Endpoint de tipo Put para actualizar los datos del usuario
   ruta.put('/:email', (req, res) => {
-    const {error, value} = schema.validate({nombre: req.body.nombre});
+    const {error, value} = logic.schema.validate({nombre: req.body.nombre});
     if(!error){
-        let resultado = actualizarUsuario(req.params.email, req.body);
+        let resultado = logic.actualizarUsuario(req.params.email, req.body);
         resultado.then(valor =>{
             res.json({
                 valor
@@ -92,21 +67,11 @@ const schema = Joi.object({
     }
 });
 
-//funcion asincronica para inactivar un usuario
 
-async function desactivarUsuario(email){
-    let usuario = await Usuario.findOneAndUpdate({"email": email},
-    {
-        $set: {
-            estado: false
-        }
-    }, {new: true});
-    return usuario;
-}
 
-////Endpoint de tipo DELETE para el recurso de los Usuario
+//Endpoint de tipo DELETE para el recurso de los Usuario
 ruta.delete('/:email', (req, res) => {
-    let resultado = desactivarUsuario(req.params.email);
+    let resultado = logic.desactivarUsuario(req.params.email);
     resultado.then(valor => {
         res.json({
             usuario : valor
@@ -118,23 +83,8 @@ ruta.delete('/:email', (req, res) => {
     });
 });
 
-//funcionalidad asincrona para listar todos los usuarios activos
 
-async function listarUsuarioActivos(){
-    let usuarios = await Usuario.find({"estado": true});
-    return usuarios;
-}
 
-//Endpoint de Tipo GET para el recurso usuarios. lista todos los usuarios activos
-ruta.get('/',(req, res) => {
-    let resultado = listarUsuarioActivos();
-    resultado.then(usuarios => {
-        res.json(usuarios)
-    }).catch(err => {
-        res.status(400).json(
-            {
-                err
-            }
-        )
-    })
-})
+module.exports = ruta;
+
+ 
